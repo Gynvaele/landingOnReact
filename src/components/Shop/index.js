2603085d107308c9046d3b467aa821fe5ed51ce3
Pasha <88pasha88@gmail.com>
2020-02-18 00:15:59 +0200

    shop structure add

--- src/components/Shop/index.js
+++ src/components/Shop/index.js
@@ -1,35 +1,92 @@
 import React from "react";
-import "../style.scss"
+import {Route, NavLink} from "react-router-dom";
+import "../style.scss";
+import {category} from "../../assets/data"
 
 const ShopHeader = () => {
-    return <div className="container">
-        <div className="col">Logo</div>
-        <div className="col">Korzina + Lang</div>
-    </div>
+    return (
+        <div className="container">
+            <div className="logo-container">Logo</div>
+            <div className="wrapper">
+                <div>
+                    <img src="" alt="cart-logo"/>
+                    <NavLink to={"/cart"}>Корзина</NavLink>
+                </div>
+                <div>
+                    <NavLink to="/" className="language">RU</NavLink>
+                    <NavLink to="/UA" className="language">UA</NavLink>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 
 const Search = () => {
-    return <div className="search">
-        <div className="container">
-            <div className="search-logo">Search logo</div>
-            <div className="search-input">
-                <input type="text" placeholder={"search"}/>
+    return (
+        <div className="search">
+            <div className="container">
+                <div className={"wrapper"}>
+                    <div className="search-logo">Search logo</div>
+                    <div className="search-input">
+                        <input type="text" placeholder={"search"}/>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    )
 };
 
+const Categories = () => {
+    return (
+        <div className="container">
+            {category.map(elm => {
+                return (
+                    <NavLink to={elm.path} className="categories-block" key={elm.id}>
+                        {elm.title}
+                    </NavLink>
+                )
+            })}
+        </div>
+    )
+};
+
+
+const ShopCategoryLayout = (props) => {
+    return (
+        <div className={"category_wrapper"}>
+            {props.children}
+        </div>
+    )
+};
+
+
+const Breadcrumbs = () => {
+    return (
+        <div className="container">
+            <div>Breadcrumbs</div>
+        </div>
+    )
+};
 
 export const Shop = () => {
     return (
         <section id="shop">
             <ShopHeader/>
             <Search/>
-            <div className="container">
-                <div className="col">col 1</div>
-                <div className="col">col 2</div>
-            </div>
+            <Categories/>
+            <Route
+                path="/"
+                render={(props) => {
+                    return (
+                        <ShopCategoryLayout>
+                            <Breadcrumbs {...props}/>
+                            <div className="container">
+                                <div>Category title</div>
+                            </div>
+                        </ShopCategoryLayout>
+                    )
+                }}/>
         </section>
     )
 };
